20f0d0eefbf761c25329f5ae4529589a5187e479
allyson7 <allysonlopes0@gmail.com>
2019-09-13 17:36:44 -0400

    finalizando

--- exercicio/index.js
+++ exercicio/index.js
@@ -12,17 +12,37 @@ nunjucks.configure('views', {
 app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'njk')
 
+const logMiddleWare = (req, res, next) => {
+  app.id = req.body.age
+  console.log(
+    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
+  )
+  if (app.id =  ) {
+
+  } else {
+    return next()
+  }
+}
+
 app.get('/', (req, res) => {
   return res.render('main')
 })
 
-app.get('/check:id', (req, res) => {
-  const x = req.query.id
-  if (x > 18) {
+app.post('/check', (req, res) => {
+  app.id = req.body.age
+  if (app.id > 18) {
     return res.redirect('/major')
   } else {
     return res.redirect('/minor')
   }
+})
+
+app.get('/major', logMiddleWare, (req, res) => {
+  return res.send(`Você é maior de idade e possui ${app.id} anos`)
+})
+
+app.get('/minor', logMiddleWare, (req, res) => {
+  return res.send(`Você é menor de idade e possui ${app.id} anos`)
 })
 
 app.listen(3000)
