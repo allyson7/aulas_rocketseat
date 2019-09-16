3d8126e89c130a87ca39bea0bd053219bd5c0ec5
Allyson Lopes <allysonlopes0@gmail.com>
2019-09-16 18:02:52 -0400

    mod2 att 16.09

--- modulo2/src/routes.js
+++ modulo2/src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const multerConfig = require('./config/multer')
+const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  return res.send('Hello world')
-})
+const UserController = require('./app/controllers/UserController')
+
+routes.get('/signup', UserController.create)
+routes.post('/signup', UserController.store)
 
 module.exports = routes
