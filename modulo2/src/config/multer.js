9f40c15e7593f7370803b85db2895a942239fbdb
Allyson Lopes <allysonlopes0@gmail.com>
2019-09-17 12:05:10 -0400

    mod2 att3

--- modulo2/src/config/multer.js
+++ modulo2/src/config/multer.js
@@ -3,13 +3,13 @@ const crypto = require('crypto')
 const multer = require('multer')
 
 module.exports = {
-  storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'temp', 'uploads'),
-    filename: (req, file, cb) => {
+  storage: multer.diskStorage({ // salvar imagens no disco
+    destination: path.resolve(__dirname, '..', '..', 'temp', 'uploads'), // local salvamento
+    filename: (req, file, cb) => { // qual nome os arquivos vão ser salvos
       crypto.randomBytes(16, (err, raw) => {
         if (err) return cb(err)
 
-        cb()
+        cb(null, raw.toString('hex') + path.extname(file.originalname))
       })
     }
   })
