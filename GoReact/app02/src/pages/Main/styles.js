94d873a1419b60887d71941b0353480bfcb6ee49
Allyson Lopes <allysonlopes0@gmail.com>
2019-09-20 18:22:55 -0400

    app02 finalizado

---
+++ GoReact/app02/src/pages/Main/styles.js
@@ -0,0 +1,47 @@
+/* eslint-disable arrow-parens */
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 60px;
+`;
+
+export const Form = styled.form`
+  margin-top: 20px;
+  width: 100%;
+  max-width: 400px;
+  display: flex;
+
+  input {
+    flex: 1;
+    height: 55px;
+    padding: 0 20px;
+    font-size: 18px;
+    color: #444;
+    background: #fff;
+    border-radius: 3px;
+
+    border: ${props => (props.withError ? '2px solid #F00' : 0)};
+    background: ${props => (props.withError ? '#ffdede' : '#FFF')};
+  }
+
+  button {
+    height: 55px;
+    width: 80px;
+    padding: 0 20px;
+    margin-left: 10px;
+    background: #63f5b0;
+    color: #fff;
+    border: 0;
+    font-size: 20px;
+    font-weight: bold;
+    border-radius: 3px;
+
+    &:hover {
+      background: #52d89f;
+      cursor: pointer;
+    }
+  }
+`;
