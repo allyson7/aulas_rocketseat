e64e5375c3ed74e259847230bc3a88670e673f18
Allyson Lopes <allysonlopes0@gmail.com>
2019-10-21 18:05:55 -0400

    aula 6 gonative

--- GoNative/mod03/src/TodoList.js
+++ GoNative/mod03/src/TodoList.js
@@ -3,22 +3,22 @@ import React from 'react';
 import {View, Text, Button} from 'react-native';
 
 import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as TodosActions from '~/store/actions/todos';
 
 // import { Container } from './styles';
 
-const TodoList = ({todos, dispatch}) => (
+const TodoList = ({todos, addTodo, markAsCompleted}) => (
   <View style={{flex: 1, backgroundColor: '#fff', justifyContent: 'center'}}>
     {todos.map(todo => (
       <Text
+        onPress={() => markAsCompleted(todo.id)}
         style={{textDecorationLine: todo.completed ? 'line-through' : 'none'}}
         key={todo.id}>
         {todo.text}
       </Text>
     ))}
-    <Button
-      onPress={() => dispatch({type: 'ADD_TODO', text: 'Novo todo'})}
-      title="Adicionar todo"
-    />
+    <Button onPress={addTodo} title="Adicionar todo" />
   </View>
 );
 
@@ -26,4 +26,10 @@ const mapStateToProps = state => ({
   todos: state,
 });
 
-export default connect(mapStateToProps)(TodoList); //high order components
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(TodosActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(TodoList); //high order components
